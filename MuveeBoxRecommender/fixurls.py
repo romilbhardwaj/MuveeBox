82285a27f2f029871bf9fca9566cc8559922537b
Romil <romil.bhardwaj@gmail.com>
2013-12-04 01:42:31 +0530

    Moviefetcher fixed to include both omdb and mymovieapi. Not found movies are added to another file.

--- MuveeBoxRecommender/fixurls.py
+++ MuveeBoxRecommender/fixurls.py
@@ -1,28 +1,39 @@
 import json
 import urllib2
 
-f = open('u.item', 'r')
-ff = open('ufixed.item', 'w')
+readdb = open('u.item', 'r')
+fixeddb = open('ufixed.item', 'w')
+notfoundfile = open('unotfound.item', 'w')
 
-for line in f:
+for line in readdb:
 	ogline = line
 	try:
 		line=line.split('|')
-		print line
-		tofind = line[1]
-		searchurl="http://mymovieapi.com/?title="+tofind+"&type=json&plot=simple&episode=1&limit=1&yg=0&mt=none&lang=en-US&offset=&aka=simple&release=simple&business=0&tech=0"
+		tofind = line[1][:-6].replace(" ", "%20")
+		tofind = "".join(tofind.split())
+		searchurl="http://www.omdbapi.com/?t=" + tofind
+		print tofind
 		movielist = json.load(urllib2.urlopen(searchurl))
-		imdbmovie = movielist[0]
-		line[4]=imdbmovie['imdb_url']
-		line.insert(1, imdbmovie['imdb_id'])
-		line = str("|".join(line))
-		print type(line)
-		ff.write(line)
-		print line
-	except ValueError:
-			print "No IMDB Data found for " + ogline
-			ff.write(ogline)
-	except KeyError:
-			print "No IMDB Data found for " + ogline
-			ff.write(ogline)
-
+		if(movielist['Response']=="False"):
+			print "Not found in omdb, Searching in MyMovieApi"
+			searchurl="http://mymovieapi.com/?title="+tofind+"&type=json&plot=simple&episode=1&limit=1&yg=0&mt=none&lang=en-US&offset=&aka=simple&release=simple&business=0&tech=0"
+			movielist = json.load(urllib2.urlopen(searchurl))
+			try:
+				if (movielist['code']==404):
+					print "No IMDB Data found for " + tofind
+					notfoundfile.write(ogline)
+					continue
+			except TypeError:
+				imdbmovie = movielist[0]
+				line[4]=imdbmovie['imdb_url']
+				line.insert(1, imdbmovie['imdb_id'])
+				line = str("|".join(line))
+				fixeddb.write(line)
+		else:
+			imdbmovie = movielist
+			line[4]="http://www.imdb.com/title/" + imdbmovie['imdbID']
+			line.insert(1, imdbmovie['imdbID'])
+			line = str("|".join(line))
+			fixeddb.write(line)
+	except NameError:
+		print "NameError. This should not happen man!"
